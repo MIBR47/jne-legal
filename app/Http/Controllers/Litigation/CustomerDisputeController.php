a8fbfc9bc73b8f7187a71818e46e122fc3673cb7
MIBR47 <ilhambachtiar47@gmail.com>
2022-02-09 15:50:35 +0700

    benerin upload file costumer dispute

--- app/Http/Controllers/Litigation/CustomerDisputeController.php
+++ app/Http/Controllers/Litigation/CustomerDisputeController.php
@@ -49,11 +49,13 @@ class CustomerDisputeController extends Controller
             'file_other_evidence' => 'required',
         ]);
 #
-        $validatedData2 = $request->validate([
-            // 'id' => 'required',
+        // $validatedData2 = $request->validate([
+        //     // 'id' => 'required',
             
-        ]);
+        // ]);
         // $validatedData[;'']
+
+        // $data = $request->all();
 
         $name = $request->file('file_witness_testimony')->getClientOriginalName();
         $name2 = $request->file('file_letter_document')->getClientOriginalName();
@@ -63,31 +65,39 @@ class CustomerDisputeController extends Controller
         $name6 = $request->file('file_document_completeness')->getClientOriginalName();
         $name7 = $request->file('file_other_evidence')->getClientOriginalName();
         
-        $path = $request->file('file_witness_testimony')->store('public/files');
-        $path2= $request->file('file_letter_document')->store('public/files');
-        $path3 = $request->file('file_claim_form_document')->store('public/files');
-        $path4 = $request->file('file_other_document')->store('public/files');
-        $path5 = $request->file('file_evidence')->store('public/files');
-        $path6 = $request->file('file_document_completeness')->store('public/files'); 
-        $path7 = $request->file('file_other_evidence')->store('public/files'); 
+        // $path = $request->file('file_witness_testimony')->store('public/files');
+        // $path2= $request->file('file_letter_document')->store('public/files');
+        // $path3 = $request->file('file_claim_form_document')->store('public/files');
+        // $path4 = $request->file('file_other_document')->store('public/files');
+        // $path5 = $request->file('file_evidence')->store('public/files');
+        // $path6 = $request->file('file_document_completeness')->store('public/files'); 
+        // $path7 = $request->file('file_other_evidence')->store('public/files'); 
 
-        $save = new CustomerDispute;
+        $data['file_witness_testimony'] = $request->file('file_witness_testimony')->storeAs('public/files/file_witness_testimony',$name,'public');
+        $data['file_letter_document'] = $request->file('file_letter_document')->storeAs('public/files/file_letter_document',$name2,'public');
+        $data['file_claim_form_document'] = $request->file('file_claim_form_document')->storeAs('public/files/file_claim_form_document',$name3,'public');
+        $data['file_other_document'] = $request->file('file_other_document')->storeAs('public/files/file_other_document',$name4,'public');
+        $data['file_evidence'] = $request->file('file_evidence')->storeAs('public/files/file_evidence',$name5,'public');
+        $data['file_document_completeness'] = $request->file('file_document_completeness')->storeAs('public/files/file_document_completeness',$name6,'public');
+        $data['file_other_evidence'] = $request->file('file_other_evidence')->storeAs('public/files/file_other_evidence',$name7,'public');
+
+        // $save = new CustomerDispute;
  
-        $save->name = $name;
-        $save->name = $name2;
-        $save->name = $name3;
-        $save->name = $name4;
-        $save->name = $name5;
-        $save->name = $name6;
-        $save->name = $name7;
+        // $save->name = $name;
+        // $save->name = $name2;
+        // $save->name = $name3;
+        // $save->name = $name4;
+        // $save->name = $name5;
+        // $save->name = $name6;
+        // $save->name = $name7;
 
-        $save->path = $path;
-        $save->path = $path2;
-        $save->path = $path3;
-        $save->path = $path4;
-        $save->path = $path5;
-        $save->path = $path6;
-        $save->path = $path7;
+        // $save->path = $path;
+        // $save->path = $path2;
+        // $save->path = $path3;
+        // $save->path = $path4;
+        // $save->path = $path5;
+        // $save->path = $path6;
+        // $save->path = $path7;
 
         // UploadFile::create($validatedData2);
         CustomerDispute::create($validatedData);
